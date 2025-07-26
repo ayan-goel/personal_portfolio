7f687e31a7d692ec6376d29a4bd798136476bc4f
ayan-goel <agoel320@gatech.edu>
2025-07-26 14:33:02 -0400

    content updates

--- src/app/experience/page.js
+++ src/app/experience/page.js
@@ -170,7 +170,7 @@ export default function Experience() {
       description: [
         "GPA: 4.0/4.0",
         "Concentration: Artificial Intelligence and Computer Networking",
-        "Relevant Coursework: Data Structures & Algorithms, Object-Oriented Programming, Database Systems, Computer Organization and Programming, Introduction to AI, Objects and Design, Discrete Math for CS, Linear Algebra",
+        "Relevant Coursework: Data Structures & Algorithms, Design & Analysis of Algorithms, Object-Oriented Programming, Database Systems, Computer Organization and Programming, Introduction to AI, Objects and Design, Discrete Math for CS, Linear Algebra",
         "Dean's List/Faculty Honors: Fall 2024"
       ]
     },
